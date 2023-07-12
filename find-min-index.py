71aa3dcf2409e2ba2940d916a6162954c501dd38
Vagner Zampieri <vgzampieri@gmail.com>
2023-07-12 17:24:27 -0300

    update informations

--- find-min-index.py
+++ find-min-index.py
@@ -2,15 +2,16 @@
 def find_min_index(arr):
     # 1. Set the min to the first element of the array
     min = arr[0]
+    # 2. Set the min_index to the first index of the array
     min_index = 0
 
-    # 2. Loop through the array
+    # 3. Loop through the array
     for i in range(1, len(arr)):
-        # 3. Check if the current element is lower than the min
+        # 4. Check if the current element is lower than the min
         if arr[i] < min:
-            # 4. If so, set the min to the current element
+            # 5. If so, set the min to the current element
             min = arr[i]
-            # 5. Set the min_index to the current index
+            # 6. Set the min_index to the current index
             min_index = i
 
     return min_index
